61774901b1d5b558fff8627879ce2ce450207639
Olena <alyonaeremen@gmail.com>
2023-03-06 13:23:15 +0000

    Add variables to store data

---
+++ assets/ind.js
@@ -0,0 +1,8 @@
+let randomNumber=Math.floor(Math.random()*100)+1;
+let guesses=document.querySelector(".guesses");
+let lastResult=document.querySelector(".lastResult");
+let lowOfHi=document.querySelector(".lowOrHi");
+let guessField=document.querySelector(".guessField");
+let guessSubmit=document.querySelector(".guessSubmit");
+let guessCount=1;
+let resetButton;
