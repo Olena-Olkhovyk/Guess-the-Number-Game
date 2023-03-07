15cb3f35be23ac90ac0570bc664e085e8653afde
Olena <alyonaeremen@gmail.com>
2023-03-07 09:23:17 +0000

    Ada functin,, which allows users to stara new game

--- assets/ind.js
+++ assets/ind.js
@@ -40,4 +40,13 @@ function checkGuess(){
 
 guessSubmit.addEventListener("click", checkGuess);
 
+function setGameOver(){
+    guessField.disabled=true;
+    guessSubmit.disabled=true;
+    resetButton=document.createElement('button');
+    resetButton.textContent='Start new game';
+    document.body.append(resetButton);
+    resetButton.addEventListener("click",resetGame);
+}
+
 
